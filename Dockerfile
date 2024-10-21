1b8a98e3852db2514cb13862b14339d285f236cc
sebaschb <sebastianchaconbenitez@gmail.com>
2024-10-20 21:30:41 -0500

    Refactor Docker setup and migrate FastAPI application to main.py

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
+# Etapa de construcción
 FROM debian:12-slim AS build
 RUN apt-get update && \
     apt-get install --no-install-suggests --no-install-recommends --yes python3-venv gcc libpython3-dev && \
     python3 -m venv /venv && \
     /venv/bin/pip install --upgrade pip setuptools wheel
 
+# Instalación de dependencias
 FROM build AS build-venv
-
 COPY requirements.txt /requirements.txt
 
 ENV PYTHONDONTWRITEBYTECODE 1
@@ -14,10 +15,14 @@ ENV PYTHONUNBUFFERED 1
 RUN /venv/bin/pip install --disable-pip-version-check -r /requirements.txt && \
     /venv/bin/pip install debugpy 
 
+# Etapa de producción
 FROM gcr.io/distroless/python3-debian12 as production
+USER 1000
+
 COPY --from=build-venv /venv /venv
 
 COPY . /app
+
 WORKDIR /app
 
 EXPOSE 8080
---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+from fastapi import FastAPI
+from app.routes import router as incidente_router
+from app.database import init_db, engine  # Importa la función init_db y el engine
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+
+app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    if os.getenv("TESTING") != "True":
+        init_db(engine)  # Inicializa la base de datos y crea las tablas
+    yield
+    # Este código se ejecuta cuando la aplicación se apaga
+
+# Inicializa la aplicación FastAPI usando lifespan
+app = FastAPI(lifespan=lifespan)
+
+# Incluir el router
+app.include_router(incidente_router)
+
+app.include_router(incidente_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todos los orígenes, puedes restringirlo a ciertos dominios en producción
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, PUT, DELETE, OPTIONS, etc.)
+    allow_headers=["*"],  # Permite todos los encabezados
+)
+
