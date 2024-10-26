7bad8ec002b4d1512f420e691395c85c4e21a592
santiago <santiagowilchesv@gmail.com>
2024-10-26 07:56:13 -0500

    Added pub/sub configuration

--- test/conftest.py
+++ test/conftest.py
@@ -23,9 +23,6 @@ def session_fixture():
     with Session(engine) as session:
         yield session
 
-    with Session(engine_replica) as session:
-        yield session
-        
     engine.dispose()
     engine_replica.dispose()
 
